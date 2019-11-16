575981594455984d85a42ae020f383096206da3f
rileyrichter <bparker@gmail.com>
2019-11-16 15:25:03 -0600

    with rest

--- ghibli/scripts.js
+++ ghibli/scripts.js
@@ -10,22 +10,22 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://webflow-node-api.herokuapp.com/status', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(status => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = status.name;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      movie.description = status.site.substring(0, 300);
+      p.textContent = `${status.publish}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
