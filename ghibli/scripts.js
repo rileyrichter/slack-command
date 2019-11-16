8815b21f90d488a81fc408f3ddffbf9c455e0fc7
rileyrichter <bparker@gmail.com>
2019-11-16 15:25:21 -0600

    as

--- ghibli/scripts.js
+++ ghibli/scripts.js
@@ -34,7 +34,7 @@ request.onload = function () {
   } else {
     const errorMessage = document.createElement('marquee');
     errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
+    app.appendChild(errorMessage); 
   }
 }
 
